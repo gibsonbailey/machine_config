0d421ae8e693be59a97b396a643ace97431b9c9d
gibsonbailey <baileytrefts@gmail.com>
2023-03-08 09:34:29 -0800

    Configure copilot and catpuccin.

---
+++ .config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,12 @@
+require("copilot").setup({
+  panel = {
+    enabled = false,
+  },
+  suggestion = {
+    debounce = 0,
+    auto_trigger = true,
+    keymap = {
+      accept = '<TAB>',
+    }
+  },
+})
