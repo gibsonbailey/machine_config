5c473e29ff2873821f0be6107021f982636317de
gibsonbailey <baileytrefts@gmail.com>
2024-02-03 16:18:41 -0800

    Change some nvim plugin versions.

--- .config/nvim/lua/gibsonbailey/packer.lua
+++ .config/nvim/lua/gibsonbailey/packer.lua
@@ -1,81 +1,77 @@
 -- Many of these plugins are configured in the .config/nvim/after/plugins directory.
-
 -- To Sync this file run the following nvim commands:
 -- :so %
 -- :PackerSync
-
 -- To install formatters, run :Mason and navigate to the formatters section
 -- Install the formatters you want to use
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 -- Packer is for plugin management
 return require('packer').startup(function(use)
-  -- packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Search
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
-
-  -- Colors
-  use { "catppuccin/nvim", as = "catppuccin" }
-
-  -- Syntax highlighting
-  use('nvim-treesitter/nvim-treesitter', { run = ':tsupdate' })
-
-  -- LSP support
-  use {
-    'vonheikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- lsp support
-      { 'neovim/nvim-lspconfig' },             -- required
-      -- Mason is used to manage installed LSPs
-      { 'williamboman/mason.nvim' },           -- optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- optional
-
-      -- autocompletion
-      { 'hrsh7th/nvim-cmp' },         -- required
-      { 'hrsh7th/cmp-nvim-lsp' },     -- required
-      { 'hrsh7th/cmp-buffer' },       -- optional
-      { 'hrsh7th/cmp-path' },         -- optional
-      { 'saadparwaiz1/cmp_luasnip' }, -- optional
-      { 'hrsh7th/cmp-nvim-lua' },     -- optional
-
-      -- snippets
-      { 'l3mon4d3/luasnip' },             -- required
-      { 'rafamadriz/friendly-snippets' }, -- optional
+    -- Search
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        requires = {{'nvim-lua/plenary.nvim'}}
     }
-  }
 
-  -- Quick line commenting with `gcc` and block commenting with `V (highlight block) gc`
-  use 'tpope/vim-commentary'
-  -- TODO:
-  -- Make comments in JSX work better. Maybe tsserver can help?
-  -- Make this easier than `gcc`. It would feel better to have a single keystroke (with a modifier)
+    -- Colors
+    use {"catppuccin/nvim", as = "catppuccin"}
 
-  -- File Tree
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-  }
+    -- Syntax highlighting
+    use {'nvim-treesitter/nvim-treesitter', tag = 'v0.9.1'}
 
-  -- Buffer "tabs" to see which buffers are open
-  use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+    -- LSP support
+    use {
+        'vonheikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- lsp support
+            {'neovim/nvim-lspconfig'}, -- required
+            -- Mason is used to manage installed LSPs
+            {'williamboman/mason.nvim'}, -- optional
+            {'williamboman/mason-lspconfig.nvim'}, -- optional
+            -- autocompletion
+            {'hrsh7th/nvim-cmp'}, -- required
+            {'hrsh7th/cmp-nvim-lsp'}, -- required
+            {'hrsh7th/cmp-buffer'}, -- optional
+            {'hrsh7th/cmp-path'}, -- optional
+            {'saadparwaiz1/cmp_luasnip'}, -- optional
+            {'hrsh7th/cmp-nvim-lua'}, -- optional
+            -- snippets
+            {'l3mon4d3/luasnip'}, -- required
+            {'rafamadriz/friendly-snippets'} -- optional
+        }
+    }
 
-  -- Copilot autocompletion
-  use {
-    "zbirenbaum/copilot.lua",
-  }
+    -- Quick line commenting with `gcc` and block commenting with `V (highlight block) gc`
+    use 'tpope/vim-commentary'
+    -- TODO:
+    -- Make comments in JSX work better. Maybe tsserver can help?
+    -- Make this easier than `gcc`. It would feel better to have a single keystroke (with a modifier)
 
-  -- Formatting
-  use {
-    'mhartington/formatter.nvim'
-  }
+    -- File Tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons' -- optional, for file icons
+        }
+    }
+
+    -- Buffer "tabs" to see which buffers are open
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "v3.*",
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
+
+    -- Copilot autocompletion
+    use {"zbirenbaum/copilot.lua"}
+
+    -- Formatting
+    use {'mhartington/formatter.nvim'}
 end)
