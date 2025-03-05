54bbbc0e10e3cb5d9e1b8f8f2859b1a3826ac71c
Bailey Trefts <bailey.trefts@vantagerisk.com>
2025-03-05 13:32:28 -0800

    Add scripts for doing some common system package installs.

---
+++ secondary_installs.sh
@@ -0,0 +1,2 @@
+pyenv install 3.12
+nvm install node
---
+++ system_package_installs.sh
@@ -0,0 +1,10 @@
+brew install \
+  xz \
+  ripgrep \
+  neovim \
+  tree \
+  fzf \
+  pyenv
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
