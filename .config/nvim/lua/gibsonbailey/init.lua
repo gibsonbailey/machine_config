57f4044fae960c0f6228023bfbf687f441902c21
gibsonbailey <baileytrefts@gmail.com>
2025-04-05 19:14:40 -0700

    Update tmux config.

--- .config/nvim/lua/gibsonbailey/init.lua
+++ .config/nvim/lua/gibsonbailey/init.lua
@@ -1,6 +1,3 @@
 require("gibsonbailey.set")
 require("gibsonbailey.remap")
 require("gibsonbailey.commands")
-
--- Recognize .ino files as .cpp
-vim.api.nvim_command("autocmd BufRead,BufNewFile *.ino set filetype=cpp")
