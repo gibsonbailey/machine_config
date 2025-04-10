dee968099682e55dc1e863f5b770d2640f2d717b
Bailey Trefts <bailey.trefts@vantagerisk.com>
2025-04-09 18:46:18 -0700

    Add live grep args as default text search in nvim.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,18 +1,17 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
--- require("telescope").load_extension("live_grep_args")
+local live_grep_args = require("telescope").load_extension("live_grep_args")
 
 telescope.setup({
-	-- extensions = {
-	-- 	live_grep_args = {
-	-- 		auto_quoting = true,
-	-- 	},
-	-- },
+	extensions = {
+		live_grep_args = {
+			auto_quoting = true,
+		},
+	},
 })
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
--- vim.keymap.set('n', '<leader>ps', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ps", live_grep_args.live_grep_args, {})
 vim.keymap.set("n", "<leader><leader>", builtin.lsp_dynamic_workspace_symbols, {})
 vim.keymap.set("n", "<leader>s", builtin.buffers, {})
